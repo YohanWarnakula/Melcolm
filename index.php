7a6dcc88ef48a22809078f8ad41449da2ec763a4
YohanWarnakula <warnakulayohan@gmail.com>
2024-04-18 13:10:56 +0530

    create index-style.css file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/theme.css">
+</head>
+<body>
+<header class="header">
+      <a href="#" class="logo">Melcolm Photography</a>
+      <nav class="navbar">
+        <a href=".pages/index.html" class="active">Home</a>
+        <a href=".pages/about.html">About us</a>
+        <a href=".pages/menu.html">Menu</a>
+        <a href=".pages/contact.html">Contact Us</a>
+      </nav>
+    </header>
+</body>
+</html>
