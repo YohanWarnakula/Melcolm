b2f69f7634f2abfca5cc7c7e698c673f27143fbb
Myth <ashenjayarathnedev@gmail.com>
2024-04-18 18:37:21 +0530

    Create separate files for header and footer components

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/theme.css">
+    <link rel="stylesheet" href="assets/css/common.css">
+</head>
+<body>
+    <?php include('includes/header.php') ?>
+    
+    <h1>Index Page Section</h1>
+
+    <?php include('includes/footer.php') ?>
+</body>
+</html>
